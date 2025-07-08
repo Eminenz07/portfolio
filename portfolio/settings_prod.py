fec9e398f4bb178a03de717261e828d4f826f30d
Eminence <eminenceadeoluwa@gmail.com>
2025-07-08 18:44:28 +0100

    Debug logs

--- portfolio/settings_prod.py
+++ portfolio/settings_prod.py
@@ -2,7 +2,7 @@ from .settings import *
 import os
 
 # Production settings
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "localhost",
@@ -45,3 +45,17 @@ DATABASES = {
 
 USE_X_FORWARDED_HOST = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'WARNING',
+    },
+}
