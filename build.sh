f9c0ab77c9e766cf88f6fd77988f3e6a2eb3234b
Eminenz07 <eminenceadeoluwa@gmail.com>
2026-08-19 04:07:30 +0100

    feat: auto-configure superuser eminence in build.sh

--- build.sh
+++ build.sh
@@ -10,3 +10,6 @@ python manage.py collectstatic --noinput
 
 # Run migrations
 python manage.py migrate
+
+# Auto-configure admin superuser and default profile on deploy
+python manage.py shell -c "from django.contrib.auth.models import User; u, created = User.objects.get_or_create(username='eminence', defaults={'email': 'emmyadeoluwa@gmail.com', 'is_superuser': True, 'is_staff': True}); u.set_password('admin12345'); u.is_superuser = True; u.is_staff = True; u.save(); from Base.models import SiteProfile; SiteProfile.objects.get_or_create(pk=1)"
